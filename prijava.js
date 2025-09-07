560f59b7e03662f08a82fd20708115f452551860
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-09-07 18:02:23 +0200

    Update prijava.js

--- prijava.js
+++ prijava.js
@@ -1,9 +1,6 @@
 const bcrypt = require('bcrypt');
 const { User } = require('./mongo');  // Preporučujem da koristiš User model direktno
 
-// Aktivne sesije
-const activeUsers = new Set();
-
 // Funkcija za registraciju
 async function register(req, res, io) {
     const { username, password } = req.body;
@@ -34,21 +31,11 @@ async function login(req, res, io) {
         return res.status(400).send('Username and password are required.');
     }
 
-    // Provera da li Radio Galaksija i R-Galaksija pokušavaju istovremeno
-    if (
-        (username === 'Radio Galaksija' && activeUsers.has('R-Galaksija')) ||
-        (username === 'R-Galaksija' && activeUsers.has('Radio Galaksija'))
-    ) {
-        return res.status(403).send('Radio Galaksija i R-Galaksija ne mogu biti ulogovani istovremeno.');
-    }
-
     try {
         const user = await User.findOne({ username });
         if (user && await bcrypt.compare(password, user.password)) {
             const role = user.role;
             const socket = io.sockets.sockets.get(socketId);  // Pronalaženje socket-a po ID-u
-
-            activeUsers.add(username);
 
             // Emitovanje prijavljenom korisniku sa njegovim role podacima
             if (socket) {
