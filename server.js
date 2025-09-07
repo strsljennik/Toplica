e99c0bd4e54f40e734999af18c5d59f5d4549013
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-09-07 13:58:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get('/metrics', (req, res) => {
 });
 
 // Lista autorizovanih i banovanih korisnika
-const authorizedUsers = new Set(['Radio Galaksija', 'ZI ZU', '*__X__*']);
+const authorizedUsers = new Set(['Radio Galaksija','R-Galaksija', 'ZI ZU', '*__X__*']);
 const bannedUsers = new Set();
 
 // Skladištenje informacija o gostima
@@ -223,4 +223,5 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server je pokrenut na portu ${PORT}`);
 });
+
 
