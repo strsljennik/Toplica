45646b0dc81a098e78b4db487b89dcf8de66f3ee
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-09-23 05:52:53 +0200

    Update slikemodul.js

--- slikemodul.js
+++ slikemodul.js
@@ -1,6 +1,7 @@
 let io;
 let newImage = [];
 let userImages = {}; // Mapa korisničkih slika
+const authorizedUsers = new Set(['Radio Galaksija','R-Galaksija', 'ZI ZU', '*___F117___*', '*__X__*', '𝕯𝖔𝖈𝖙𝖔𝖗 𝕷𝖔𝖛𝖊', 'Dia']);
 
 // Funkcija za setovanje io objekta
 function setSocket(serverIo) {
@@ -14,7 +15,9 @@ function setSocket(serverIo) {
         socket.on('add-image', (imageSource, position, dimensions) => {
             if (!imageSource || !position || !dimensions) return;
 
+            const id = 'img-' + Date.now() + '-' + Math.floor(Math.random() * 1000); // jedinstveni ID
             const image = {
+                id,
                 imageUrl: imageSource,
                 position: position,
                 dimensions: dimensions
@@ -23,15 +26,11 @@ function setSocket(serverIo) {
             newImage.push(image);
             userImages[socket.id].push(image);
 
-            io.emit('display-image', {
-                imageUrl: imageSource,
-                position: position,
-                dimensions: dimensions
-            });
+            io.emit('display-image', image);
         });
 
         socket.on('update-image', (data) => {
-            const image = newImage.find(img => img.imageUrl === data.imageUrl);
+            const image = newImage.find(img => img.id === data.id);
             if (image) {
                 image.position = data.position;
                 image.dimensions = data.dimensions;
@@ -39,24 +38,23 @@ function setSocket(serverIo) {
             io.emit('sync-image', data);
         });
 
-        socket.on('remove-image', (imageUrl) => {
-            const index = newImage.findIndex(img => img.imageUrl === imageUrl);
+        socket.on('remove-image', (id) => {
+            const index = newImage.findIndex(img => img.id === id);
             if (index !== -1) {
                 newImage.splice(index, 1);
-                userImages[socket.id] = userImages[socket.id].filter(img => img.imageUrl !== imageUrl);
+                userImages[socket.id] = userImages[socket.id].filter(img => img.id !== id);
             }
             io.emit('update-images', newImage);
         });
 
         socket.on('delete-all', (username) => {
-    // Provera da li je korisnik u autorizovanoj grupi
-    if (authorizedUsers.has(username)) { 
-        newImage = []; // Briše sve slike
-        userImages = {}; // Briše sve korisničke slike
-        io.emit('update-images', newImage); // Obavesti sve klijente o brisanju slika
-    } else {
-        socket.emit('error', 'Nemate privilegije da obrišete slike!'); // Ako korisnik nije privilegovan
-      }
+            if (authorizedUsers.has(username)) { 
+                newImage = []; // Briše sve slike
+                userImages = {}; // Briše sve korisničke slike
+                io.emit('update-images', newImage);
+            } else {
+                socket.emit('error', 'Nemate privilegije da obrišete slike!');
+            }
         });
     });
 }
