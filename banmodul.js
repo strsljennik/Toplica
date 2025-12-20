b737bf9d434f92761548fdad2c15b554ced337f8
strsljennik <strsljennik@gmail.com>
2025-12-20 08:42:24 +0100

    Implement MongoDB for user ban management
    Refactor ban management to use MongoDB for persistence and add init event for client ban checks.

--- banmodul.js
+++ banmodul.js
@@ -1,48 +1,64 @@
+const { getDB } = require('./mongo'); // funkcija koja vraća connected DB
 const userSockets = new Map(); // socket.id → username
 
-function setupSocketEvents(io, guests, bannedUsers, authorizedUsers) {
-    io.on('connection', (socket) => {
+async function isBanned(clientId) {
+    const db = getDB();
+    const ban = await db.collection('fronban').findOne({ clientId });
+    return !!ban;
+}
+
+async function banClient(clientId) {
+    const db = getDB();
+    await db.collection('fronban').updateOne(
+        { clientId },
+        { $set: { clientId, date: new Date() } },
+        { upsert: true }
+    );
+}
+
+async function unbanClient(clientId) {
+    const db = getDB();
+    await db.collection('fronban').deleteOne({ clientId });
+}
+
+function setupSocketEvents(io, guests, authorizedUsers) {
+    io.on('connection', async (socket) => {
         const nickname = guests[socket.id];
 
-        // Odmah po konekciji pošalji listu banovanih korisnika ovom socketu
-        bannedUsers.forEach(banNick => {
-            io.to(socket.id).emit('userBanned', banNick);
+        // kada se front pošalje init sa clientId
+        socket.on('init', async clientId => {
+            // proveri u bazi da li je banovan
+            if (await isBanned(clientId)) {
+                socket.emit('userBanned', clientId);
+            }
         });
 
         // Praćenje prijavljenih korisnika (za autorizaciju ban/unban)
         socket.on('userLoggedIn', (username) => {
             userSockets.set(socket.id, username);
             guests[socket.id] = username;
-
-            // Pošalji svima novu listu korisnika
             io.emit('updateGuestList', Object.values(guests));
-
-            // Pošalji svim klijentima sve trenutne banove
-            bannedUsers.forEach(banNick => {
-                io.emit('userBanned', banNick);
-            });
         });
 
         // Ban/unban funkcija (samo autorizovani)
-        socket.on('banUser', (targetNickname) => {
+        socket.on('banUser', async (targetClientId) => {
             const username = userSockets.get(socket.id);
             if (!authorizedUsers || !authorizedUsers.has(username)) return;
-            if (targetNickname === '*__X__*') return;
 
-            if (bannedUsers.has(targetNickname)) {
-                bannedUsers.delete(targetNickname);
-                io.emit('userUnbanned', targetNickname);
+            if (await isBanned(targetClientId)) {
+                await unbanClient(targetClientId);
+                io.emit('userUnbanned', targetClientId);
             } else {
-                bannedUsers.add(targetNickname);
-                io.emit('userBanned', targetNickname);
+                await banClient(targetClientId);
+                io.emit('userBanned', targetClientId);
             }
         });
 
         // Chat blokada za banovane
-        socket.on('chatMessage', (msg) => {
-            const currentNickname = guests[socket.id];
-            if (bannedUsers.has(currentNickname)) return;
-            io.emit('chatMessage', currentNickname, msg);
+        socket.on('chatMessage', async (msg) => {
+            const clientId = socket.handshake.query.clientId; // ili iz init
+            if (await isBanned(clientId)) return;
+            io.emit('chatMessage', guests[socket.id], msg);
         });
     });
 }
